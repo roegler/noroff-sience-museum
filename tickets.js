61f827ac20fc98323e9b22ee5caeddca089ac6cf
Marie Røgler <marie-r@hotmail.no>
2019-11-11 11:40:23 +0100

    bugfix

--- tickets.js
+++ tickets.js
@@ -1,10 +1,126 @@
-// [.ticket-button, .ticket-button, .ticket-button, .ticket-button]
-var ticketButtonContainers = document.getElementsByClassName('ticket-button')
+// Adults tickets
 
-for (var i = 0; i < ticketButtonContainers.length; i++) {
-    var container = ticketButtonContainers[i]
-    console.log(container)
+var adultsPlusButton = document.getElementById('adults-ticket-qty-plus')
+console.log(adultsPlusButton)
 
+var adultsMinusButton = document.getElementById('adults-ticket-qty-minus')
+console.log(adultsMinusButton)
+
+var adultsQtyElement = document.getElementById('adults-qty')
+console.log(adultsQtyElement)
+
+adultsPlusButton.onclick = function() {
+    console.log('Plus button clicked.')
+
+    var currentQty = parseInt(adultsQtyElement.innerHTML)
+    adultsQtyElement.innerHTML = currentQty + 1
+}
+
+adultsMinusButton.onclick = function() {
+    console.log('Minus button clicked.')
+
+    var currentQty = parseInt(adultsQtyElement.innerHTML)
+    adultsQtyElement.innerHTML = currentQty - 1
+
+    if (currentQty > 0) {
+        adultsQtyElement.innerHTML = currentQty - 1
+    }
+
+}
+
+// Seniors tickets
+
+var seniorsPlusButton = document.getElementById('seniors-ticket-qty-plus')
+console.log(seniorsPlusButton)
+
+var seniorsMinusButton = document.getElementById('seniors-ticket-qty-minus')
+console.log(seniorsMinusButton)
+
+var seniorsQtyElement = document.getElementById('seniors-qty')
+console.log(seniorsQtyElement)
+
+seniorsPlusButton.onclick = function() {
+    console.log('Plus button clicked.')
+
+    var currentQty = parseInt(seniorsQtyElement.innerHTML)
+    seniorsQtyElement.innerHTML = currentQty + 1
+}
+
+seniorsMinusButton.onclick = function() {
+    console.log('Minus button clicked.')
+
+    var currentQty = parseInt(seniorsQtyElement.innerHTML)
+    seniorsQtyElement.innerHTML = currentQty - 1
+
+    if (currentQty > 0) {
+        seniorsQtyElement.innerHTML = currentQty - 1
+    }
+
+}
+
+// Students tickets
+
+var studentsPlusButton = document.getElementById('students-ticket-qty-plus')
+console.log(studentsPlusButton)
+
+var studentsMinusButton = document.getElementById('students-ticket-qty-minus')
+console.log(studentsMinusButton)
+
+var studentsQtyElement = document.getElementById('students-qty')
+console.log(studentsQtyElement)
+
+studentsPlusButton.onclick = function() {
+    console.log('Plus button clicked.')
+
+    var currentQty = parseInt(studentsQtyElement.innerHTML)
+    studentsQtyElement.innerHTML = currentQty + 1
+}
+
+studentsMinusButton.onclick = function() {
+    console.log('Minus button clicked.')
+
+    var currentQty = parseInt(studentsQtyElement.innerHTML)
+    studentsQtyElement.innerHTML = currentQty - 1
+
+    if (currentQty > 0) {
+        studentsQtyElement.innerHTML = currentQty - 1
+    }
+
+}
+
+// Childens tickets
+
+var childrensPlusButton = document.getElementById('childrens-ticket-qty-plus')
+console.log(childrensPlusButton)
+
+var childrensMinusButton = document.getElementById('childrens-ticket-qty-minus')
+console.log(childrensMinusButton)
+
+var childrensQtyElement = document.getElementById('childrens-qty')
+console.log(childrensQtyElement)
+
+childrensPlusButton.onclick = function() {
+    console.log('Plus button clicked.')
+
+    var currentQty = parseInt(childrensQtyElement.innerHTML)
+    childrensQtyElement.innerHTML = currentQty + 1
+}
+
+childrensMinusButton.onclick = function() {
+    console.log('Minus button clicked.')
+
+    var currentQty = parseInt(childrensQtyElement.innerHTML)
+    childrensQtyElement.innerHTML = currentQty - 1
+
+    if (currentQty > 0) {
+        childrensQtyElement.innerHTML = currentQty - 1
+    }
+
+}
+
+
+
+/*
     var plusButton = container.getElementsByClassName('ticket-qty-plus')[0]
     console.log(plusButton)
 
@@ -34,3 +150,4 @@ for (var i = 0; i < ticketButtonContainers.length; i++) {
 
 
 }
+*/
